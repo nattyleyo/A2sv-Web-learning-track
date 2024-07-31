95fa683b7e7750b8bcaac0d2d4de8c40ce51353a
Natnael Meseret <natnaelmeseret5@gmail.com>
2024-07-31 16:48:41 +0300

    finalising

---
+++ Task-1/read.md
@@ -0,0 +1,6 @@
+<h1>Todolist App <h1/>
+<p>Below is a simple to-do list application using HTML, CSS, and JavaScript. I'll provide the full code for each file (HTML, CSS, and JavaScript) and an example of how you can describe this in a README file, including a screenshot of the app.<p/>
+<p>Screenshot-1<p/>
+<img src="/Task-1/asset/Screenshot-1.png" alt="" />
+<p>Screenshot-2<p/>
+<img src="/Task-1/asset/Screenshot-2.png" alt="" />
--- Task-1/todo.js
+++ Task-1/todo.js
@@ -1,1 +1,62 @@
-const 
+const btn_add = document.getElementById("add-btn");
+const btn_edit = document.getElementById("edit-btn");
+const container = document.getElementById("list-add");
+const empty = document.getElementById("empty");
+const input_data = document.getElementById("text-inp");
+
+// x.innerHTML = `<div class="title">List Added</div>`;
+var flag = 0;
+btn_add.addEventListener("click", () => {
+  flag++;
+  const newDiv = document.createElement("div");
+
+  newDiv.className = "items";
+  const content = `
+        <li class="items" key="${flag}">
+          <div class="title" id= "title">
+          <i class="fa-solid fa-feather-pointed"></i>
+          </div>
+          <p class="content" id = 'content'>${input_data.value}
+          </p>
+          <button class="btns edit-btn">
+            <i class="fa-solid fa-pen-to-square"></i>
+          </button>
+          <button class="btns del-btn">
+            <i class="fa-solid fa-trash"></i>
+          </button>
+        </li>`;
+  newDiv.innerHTML = content;
+  empty.style.display = "None";
+  container.appendChild(newDiv);
+  input_data.value = "";
+
+  const del_btns = newDiv.querySelectorAll(".del-btn");
+  del_btns.forEach((del_btn) => {
+    del_btn.addEventListener("click", () => {
+      const x = confirm(`Are you sure want to delete`);
+      if (x) {
+        container.removeChild(newDiv); // Remove the parent <div>
+        flag--;
+        if (flag < 1) {
+          empty.style.display = "flex";
+        }
+      }
+    });
+  });
+
+  const edit_btns = newDiv.querySelectorAll(".edit-btn");
+  edit_btns.forEach((edit_btn) => {
+    edit_btn.addEventListener("click", () => {
+      btn_add.style.display = "none";
+      btn_edit.style.display = "flex";
+      input_data.value = newDiv.querySelector(".items .content").textContent;
+    });
+  });
+
+  btn_edit.addEventListener("click", () => {
+    newDiv.querySelector(".items .content").textContent = input_data.value;
+    btn_add.style.display = "flex";
+    btn_edit.style.display = "none";
+    input_data.value = "";
+  });
+});

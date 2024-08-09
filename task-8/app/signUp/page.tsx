a7ed5d9b9c675ae32fdb106d0c8e36bed62ece28
Natnael Meseret <natnaelmeseret5@gmail.com>
2024-08-09 12:09:00 +0300

    g

--- task-8/app/signUp/page.tsx
+++ task-8/app/signUp/page.tsx
@@ -1,12 +1,7 @@
 import React from "react";
-import Image from "next/image";
-import imgUrl from "../../public/assets/IcongoogleIcon.svg";
-import Link from "next/link";
 import SignUpComp from "../components/SignupComp";
 const SignUp = () => {
-  return;
-  <SignUpComp />;
-  <></>;
+  return <SignUpComp />;
 };
 
 export default SignUp;
--- task-8/app/signUp/verification/page.tsx
+++ task-8/app/signUp/verification/page.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import Link from "next/link";
 import VerifyComp from "@/app/components/VerifyComp";
 
 interface OTPType {
